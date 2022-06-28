e07cdcfd702a18f15bac46c7620ee2a960a0f42a
sailendrachettri <sailendra9083@gmail.com>
2022-06-28 07:15:49 +0530

    first recursion code

--- recursion__.cpp
+++ recursion__.cpp
@@ -5,13 +5,24 @@ Author: Sailendra */
 #include <bits/stdc++.h>
 using namespace std;
 
+int tms=0;
+void print_name(string name){
+    if(tms==5) return;
+
+    cout<<name<<endl;
+    tms++;
+    print_name(name);
+
+}
+
 int main()
 {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
-    int n; cin>>n;
 
-    cout<<"Hello World!"<<n<<endl;
+    string name; cin>>name;
+    print_name(name);
+
 
     return 0;
 }
