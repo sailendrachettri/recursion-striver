a6c3d7ed778c35ab5d42fb15fd8f1485521d24f9
sailendrachettri <sailendra9083@gmail.com>
2022-06-28 06:49:06 +0530

    initial commit

---
+++ recursion__.cpp
@@ -0,0 +1,17 @@
+/* Date: 28 - 06 - 2022
+Author: Sailendra */
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int n; cin>>n;
+
+    cout<<"Hello World!"<<n<<endl;
+
+    return 0;
+}
